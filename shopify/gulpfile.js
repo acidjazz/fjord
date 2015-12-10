2ea507707b3d76dfad781fccf2b71ada3e896f9e
kevin olson <acidjazz@gmail.com>
2015-12-09 23:48:13 -0800

    initial shopify theme

---
+++ shopify/gulpfile.js
@@ -0,0 +1,72 @@
+/*
+gulp --url "http://fjordlife.myshopify.com/?key=ae7e98dcf84093ac06483c1c4dbad6f1b86fdd70dc5cfcbe456fcd214ae427c9&preview_theme_id=77151233"
+*/
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var shell       = require('gulp-shell');
+var filter      = require('gulp-filter');
+var watch       = require('gulp-watch');
+
+var minimist    = require('minimist');
+
+var knownOptions = {
+  string: 'url',
+  default: { url: process.env.NODE_ENV }
+};
+
+var options = minimist(process.argv.slice(2), knownOptions);
+
+function isChanged(file) {
+    return file.event === 'change';
+}
+
+var filterChanged = filter(isChanged);
+
+gulp.task('serve', function() {
+  browserSync.init({
+    proxy: options.url,
+    browser: "google chrome",
+    injectChanges: false, // cause of css being served from cdn
+  });
+
+  gulp.watch("assets/*.scss", ['sass']);
+});
+
+// Compile sass into CSS & auto-inject into browsers
+gulp.task('sass', function() {
+  return gulp.src("assets/*.scss")
+    .pipe(sass())
+    .pipe(gulp.dest("assets/"))
+    .pipe(shell([
+      'theme upload <%= f(file.path) %>'
+    ], {
+      templateData: {
+        f: function (s) {
+          // cut away absolute path of working dir for 'theme' cmd to work
+          return s.replace(process.cwd() + '/', '')
+        }
+      }
+    }))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('liquid', function() {
+  return gulp.src('**/*.liquid')
+    .pipe(watch('**/*.liquid'))
+    .pipe(filterChanged)
+    .pipe(shell([
+      'theme upload <%= f(file.path) %>'
+    ], {
+      templateData: {
+        f: function (s) {
+          // cut away absolute path of working dir for 'theme' cmd to work
+          return s.replace(process.cwd() + '/', '')
+        }
+      }
+    }))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve', 'liquid']);
